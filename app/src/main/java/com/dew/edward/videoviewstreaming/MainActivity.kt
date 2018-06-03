eca089034832dc54fe9467f39c481eee641c8a10
Edward <ed828a@gmail.com>
2018-06-04 08:44:41 +0930

    Initial commit

---
+++ app/src/main/java/com/dew/edward/videoviewstreaming/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.dew.edward.videoviewstreaming
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var playbackPosition = 0
+    private val realTimeStreamingUrl = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov"
+
+    private lateinit var mediaController: MediaController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mediaController = MediaController(this)
+
+        videoView.setOnPreparedListener {
+            mediaController.setAnchorView(videoContainer)
+            videoView.setMediaController(mediaController)
+            videoView.seekTo(playbackPosition)
+            videoView.start()
+        }
+
+        videoView.setOnInfoListener { player, what, extra ->
+            if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START){
+                progressBar.visibility = View.INVISIBLE
+            }
+            true
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val uri = Uri.parse(realTimeStreamingUrl)
+        videoView.setVideoURI(uri)
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        videoView.pause()
+        playbackPosition = videoView.currentPosition
+    }
+
+    override fun onStop() {
+        videoView.stopPlayback()
+        playbackPosition = 0
+
+        super.onStop()
+    }
+}
